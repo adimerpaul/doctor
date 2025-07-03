10a2d0323b09ba3700d7317de8d3053945aeae3f
adimerpaul <adimer101@gmail.com>
2025-07-03 07:18:11 -0400

    comenzando

--- back/app/Http/Controllers/CarruselController.php
+++ back/app/Http/Controllers/CarruselController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Carrusel;
 use Illuminate\Http\Request;
 
-class CarruselController extends Controller
-{
-    //
+class CarruselController extends Controller{
+    function index(){
+        return Carrusel::all();
+    }
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::get('/carrusel', [\App\Http\Controllers\CarruselController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/carrusel', [\App\Http\Controllers\CarruselController::class, 'store']);
+    Route::put('/carrusel/{id}', [\App\Http\Controllers\CarruselController::class, 'update']);
+    Route::delete('/carrusel/{id}', [\App\Http\Controllers\CarruselController::class, 'destroy']);
+});
